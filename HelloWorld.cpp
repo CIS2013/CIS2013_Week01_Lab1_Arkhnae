f1a292ef84a716cae10d16eac3e6f50635decd57
Arkhane <arkhane@okstate.edu>
2018-01-17 20:38:35 -0600

    c++ Week01
    Hello world

---
+++ HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char something;
+	cout << "Type a characterr and hit enter";
+	cin >> something;
+	cout << "You typed " << something;
+	
+
+	return 0;
+}
